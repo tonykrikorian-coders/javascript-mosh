3cd2c629f00c5a8c1719ddab83253e51ebb818c6
Tony <tony.krikorian@outlook.com>
2018-09-01 18:49:21 -0300

    Switch Case Application

--- index.js
+++ index.js
@@ -2,11 +2,27 @@
 //Sí esta entre las 12pm y las 6pm decimos: "Buenas tardes"
 //DEspues de las 6:00pm decimos :"Buenas noches"
 
-let hour = 20;
+// let hour = 11;
 
-if (hour >= 6 && hour < 12) 
-    console.log('Buenos dias');
-else if (hour >= 12 && hour < 18) 
-    console.log('Buenas tardes');
- else 
-    console.log('Buenas noches');
+// if (hour >= 6 && hour < 12) 
+//     console.log('Buenos dias');
+// else if (hour >= 12 && hour < 18) 
+//     console.log('Buenas tardes');
+//  else 
+//     console.log('Buenas noches');
+
+let role = 'moderator';
+
+switch (role) {
+    case 'guest':
+        console.log('Guest User');
+        break;
+
+    case 'moderator':
+        console.log('Moderator User');
+        break;
+        
+    default:
+        console.log('Unknow User');
+        break;
+}
