76286915c4ee3d101d24cba7dfbbd696a57e07c4
Tony <tony.krikorian@outlook.com>
2018-09-01 18:36:53 -0300

    Creación de archivos
    Se crean los archivos limpios del curso

---
+++ index.js
@@ -0,0 +1,13 @@
+//Si la hora es entre las 6am y las 12pm decimos "Buenos dias"
+//Sí esta entre las 12pm y las 6pm decimos: "Buenas tardes"
+//DEspues de las 6:00pm decimos :"Buenas noches"
+
+let hour = 20;
+if (hour >= 6 && hour < 12) {
+    console.log('Buenos dias');
+}
+else if (hour >= 12 && hour < 18) {
+    console.log('Buenas tardes');
+} else {
+    console.log('Buenas noches');
+}
