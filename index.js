a3f32f5dc4e4ca75a576f2e6c6b99bb54deb3118
Tony <tony.krikorian@outlook.com>
2018-09-02 17:51:37 -0300

    Infinite Loop

--- index.js
+++ index.js
@@ -40,9 +40,23 @@
 // }
 
 //while loop
-let i = 0;
+// let i = 0;
 
-while (i <= 5) {
-    if (i % 2 !== 0) console.log(i);
-    i++;
-}
+// while (i <= 5) {
+//     if (i % 2 !== 0) console.log(i);
+//     i++;
+// }
+
+// do-while loop
+// let i = 0;
+// do{
+//     if (i % 2 !== 0) console.log(i);
+//      i++;
+// }while (i <= 5);
+
+//infinite loops
+let i = 0;
+// while (i < 5){
+//     console.log(i);
+//     // i++;
+// }
